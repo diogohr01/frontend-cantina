c7cfb2c45687d4d80c2eebaa5f0f7c4e00a5e1d0
Diogo Rodrigues <diogorodrigues699@yahoo.com>
2024-09-18 15:12:50 -0300

    Dashboard com o ranking de produtos e o total vendido.

--- src/app/dashboard/components/orders/index.tsx
+++ src/app/dashboard/components/orders/index.tsx
@@ -1,5 +1,5 @@
 "use client"
-import {use} from 'react'
+import { use } from 'react'
 import styles from './styles.module.scss'
 import { RefreshCw } from 'lucide-react'
 import { OrderProps } from '@/lib/order.type'
@@ -14,42 +14,51 @@ interface Props {
 }
 
 export function Orders({ orders }: Props) {
-      const {isOpen, onRequestOpen} = use(OrderContext)
+      const { isOpen, onRequestOpen } = use(OrderContext)
       const router = useRouter();
 
-      async function handleDetailOrder(order_id: string){
+      async function handleDetailOrder(order_id: string) {
             console.log(order_id)
             await onRequestOpen(order_id)
-            
+
       }
-      function handleRefreshOrders(){
+      function handleRefreshOrders() {
 
             router.refresh();
             toast.success('Pedidos atualizados com sucesso')
       }
-      
+
       return (
             <>
-            <main className={styles.container}>
-                  <section className={styles.containerHeader}>
-                        <h1>Ultimos pedidos</h1>
-                        <button onClick={handleRefreshOrders} className={styles.refresh}>
-                              <RefreshCw size={24} color='#fd8a07' />
-                        </button>
-                  </section>
-
-                  <section className={styles.listOrders}>
-                        {orders.map(order => (
-                              <button className={styles.orderItem} key={order.id} onClick={()=>handleDetailOrder(order.id)}>
-                                    <div className={styles.tag}></div>
-                                    <span>Pedido: {order.table}</span>
+                  <main className={styles.container}>
+                        <section className={styles.containerHeader}>
+                              <h1>Ultimos pedidos</h1>
+                              <button onClick={handleRefreshOrders} className={styles.refresh}>
+                                    <RefreshCw size={24} color='#fd8a07' />
                               </button>
-                        ))}
-                  </section>
+                        </section>
 
-            </main>
+                        <section className={styles.listOrders}>
+                              {orders && orders.length > 0 ? (
+                                    orders.map(order => (
+                                          <button
+                                                className={styles.orderItem}
+                                                key={order.id}
+                                                onClick={() => handleDetailOrder(order.id)}
+                                          >
+                                                <div className={styles.tag}></div>
+                                                <span>Pedido: {order.table}</span>
+                                          </button>
+                                    ))
+                              ) : (
+                                    <p className={styles.notOrders}>Nenhum pedido encontrado</p>
+                              )}
 
-            {isOpen && <ModalOrder/>}
+                        </section>
+
+                  </main>
+
+                  {isOpen && <ModalOrder />}
             </>
       )
 }
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -7,11 +7,13 @@ import { useEffect, useState } from 'react';
 
 interface SoldData {
     totalValue: number;
-   
+
 }
 
+
 export default function Dashboard() {
-    const [soldData, setSoldData] = useState<SoldData | null>(null); 
+    const [soldData, setSoldData] = useState<SoldData | null>(null);
+    const [rankingProduct, setRankingProduct] = useState<{ id: string; produto: string; quantidade: number }[]>([]);
 
     async function getSold() {
         const token = getCookieClient();
@@ -22,15 +24,32 @@ export default function Dashboard() {
                     Authorization: `Bearer ${token}`
                 }
             });
-            console.log(response.data);
-            setSoldData(response.data);  
+
+            setSoldData(response.data);
         } catch (error) {
             console.error("Erro ao buscar os dados de vendas:", error);
         }
     }
 
+    async function getRankingProduct() {
+        const token = getCookieClient();
+
+        try {
+            const response = await api.get('/rankingProduct', {
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }
+            });
+            console.log(response.data.produtos);
+            setRankingProduct(response.data.produtos);
+        } catch (error) {
+            console.error("Erro ao buscar os dados do Rankind de produtos:", error);
+        }
+    }
+
     useEffect(() => {
-        getSold();  
+        getSold();
+        getRankingProduct();
     }, []);
 
     return (
@@ -41,14 +60,23 @@ export default function Dashboard() {
                         <div className={styles.tag}></div>
 
                         <span className={styles.totalVendido}>
-                            Total vendido: {soldData ? `R$${soldData.totalValue},00` : "Carregando..."}  
+                            Total vendido: {soldData ? `R$${soldData.totalValue},00` : "Carregando..."}
                         </span>
                     </div>
                 </div>
                 <div className={styles.main}>
                     <article className={styles.article}>
                         <span className={styles.rankingProdutos}>
-                            Teste
+                            <span className={styles.ranking}> Ranking de produtos</span>
+                            { rankingProduct.length > 0 ?rankingProduct.map((produto) => (
+                                <div key={produto.id} className={styles.rankingProduct}>
+                                    <div className={styles.descriptionProduct}>
+                                    <p >Produto: {produto.produto}</p>
+                                    <p >Quantidade: {produto.quantidade}</p>
+                                    </div>
+                                   
+                                </div>
+                            )): <span className={styles.noProducts}>Carregando produtos...</span>}
                         </span>
                     </article>
                     <aside className={styles.aside}>
